38ff83a50958af565b4fb7d49eaafe0ce053e0ec
José C. García Gamero <jcgarciagamero@gmail.com>
2017-03-09 22:46:37 +0100

    Update ModifyMeta.sh

--- ModifyMeta.sh
+++ ModifyMeta.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+#Lince Metadata 1.0
+#Author: José C. García Gamero.
+#Date: 2017-01-14
 
 directorio=$(pwd)
 
@@ -17,9 +21,9 @@ var2=$(echo $?)
 	
 		if [ $var3 = 1 ]; then
 		"$directorio/LinceMetadata.sh"
-		else exiftool -exif:$etiqueta="$info" $ruta
+		else exiftool -exif:$etiqueta="$info" "$ruta"
 
-		exiftool $ruta > "$directorio/lincemetadata.txt"
+		exiftool "$ruta" > "$directorio/lincemetadata.txt"
 		zenity --title="Metadatos" --text-info --filename="$directorio/lincemetadata.txt"
 		var3=$(echo $?)
 
